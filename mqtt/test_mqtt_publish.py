929014c744d9635da79d69f3d05978ce58b4cc75
Jakub Porubcansky <jakub.porubcansky@tangent.works>
2021-01-24 23:33:32 +0100

    test script update

--- mqtt/test_mqtt_publish.py
+++ mqtt/test_mqtt_publish.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import json
 import pprint
 import time
+import random as rnd
 
 # dict_msg = {
 #     "contact": True,
@@ -10,38 +11,55 @@ import time
 #     "my_field": "awesome",
 # }
 # msg = json.dumps(dict_msg)
-msg = "measurement,cpu,time_user,time_system,time_idle,time\ncpu,cpu0,42,42,42,2018-09-13T13:03:28Z"
-# msg = "19.2"
-
-pprint.pprint(msg)
-
-MQTT_HOST = "192.168.0.161"
-MQTT_PORT = 11883
-MQTT_KEEPALIVE_INTERVAL = 45
-
-count = 0
 
 
-def on_publish(client, userdata, mid):
-    print("Message Published to MQTT ...")
+# msg = "measurement,cpu,time_user,time_system,time_idle,time\ncpu,cpu0,42,42,42,2018-09-13T13:03:28Z"
+
+# pprint.pprint(msg)
+
+# MQTT_HOST = "192.168.0.161"
+# MQTT_PORT = 11883
+# MQTT_KEEPALIVE_INTERVAL = 45
+
+# count = 0
 
 
-mqttc = mqtt.Client("python_client")
-mqttc.on_publish = on_publish
-# mqttc.username_pw_set("esp1","esp1")
-mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
-
-mqttc.publish("temperature", msg)
-
-mqttc.disconnect()
-time.sleep(2)
+# def on_publish(client, userdata, mid):
+#     print("Message Published to MQTT ...")
 
 
+# mqttc = mqtt.Client("python_client")
+# mqttc.on_publish = on_publish
+# # mqttc.username_pw_set("esp1","esp1")
+# mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
 
-msg = "measurement,cpu,time_user,time_system,time_idle,time\ncpu,cpu0,100,42,42,2018-09-13T13:03:28Z"
+# mqttc.publish("temperature", msg)
+
+# mqttc.disconnect()
+# time.sleep(2)
+
+
+
+# msg = "measurement,cpu,time_user,time_system,time_idle,time\ncpu,cpu0,100,200,200,2018-09-13T13:03:28Z"
 broker_address="192.168.0.161" 
 client = mqtt.Client("python") #create new instance
+for i in range(0, 100):
+    temp = rnd.uniform(-30, 35)
+    hum = rnd.uniform(0, 100)
+    room_num = rnd.randint(1, 2)
+    msg = "temperature,humidity\n{},{}".format(temp, hum)
+    client.connect(broker_address, 11883) #connect to broker
+    client.publish("room{}".format(room_num), msg)
+    client.disconnect()
+    time.sleep(10)
+
+
+
+temp = rnd.uniform(-30, 35)
+hum = rnd.uniform(0, 100)
+room_num = rnd.randint(1, 2)
+msg = "a,b\n{},{}".format(temp, hum)
 client.connect(broker_address, 11883) #connect to broker
-# client.subscribe("house/bulbs/bulb1")
-client.publish("temperature", msg)
+client.publish("xx".format(room_num), msg)
 client.disconnect()
+time.sleep(10)
